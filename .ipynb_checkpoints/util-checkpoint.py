ae78927db06b245c3b5c3bc273268db4b26f796f
Hariharan <hari0298@gmail.com>
2023-05-01 18:44:48 +0530

    code refatctoring

---
+++ .ipynb_checkpoints/util-checkpoint.py
@@ -0,0 +1,31 @@
+import re
+import numpy as np
+from keras import backend as K
+
+# Function Definitions
+
+def remove_pattern(input_txt, pattern):
+    r = re.findall(pattern, input_txt)
+    for i in r:
+        input_txt = re.sub(i, '', input_txt)
+    return input_txt 
+
+def recall(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
